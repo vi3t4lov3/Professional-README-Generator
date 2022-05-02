9fd4686efc04aeb5ccbeb22ca53c4479e22019c4
Tu Nguyen <tunguyen@Tus-MacBookPro.local>
2022-05-01 23:22:14 -0400

    succecced add the editor using VIM on mac or linux

--- README.md
+++ README.md
@@ -1,15 +1,32 @@
 
-# Testing Project Name
+# testing
 
 ## Description
 
-hello world
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+- What was your motivation?
+- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+- What problem does it solve?
+- What did you learn?
+
 
 ## Dependencies
 
 - HTML
 - CSS
 - Javascript
+
+## installation
+
+You can run a local copy of this site by issuing the following commands. 
+```bash
+$ git clone https://github.com/vi3t4lov3/HW6-Weather-Dashboard.git
+$ cd HW6-Weather-Dashboard
+```
+
+
+## Contributing
 
 ## License
 
--- index.js
+++ index.js
@@ -1,39 +1,50 @@
 const inquire = require('inquirer');
 const fs = require('fs');
 
-const promptUser = () => {
-   return inquire.prompt([
-       { 
-        type: 'input',
-        name: 'title',
-        message: 'What is your repository name?',
-        validate: (answer) => {
-          if (answer === '') { 
-            return 'Please enter a valid title';
-          };
-          return true; // return true so it can go to next input
-        }
-       },
-       { 
-        type: 'input',
-        name: 'description',
-        message: 'Write your description'
-        }, 
-        { 
-        type: 'checkbox',
-        name: 'dependencies',
-        message: 'Technologies / Skill you using on this repository',
-        choices: ['HTML', 'CSS', 'Javascript', 'Python', 'Java'],
-        default: 'Javascript'
-        },
-        {
-        type: 'checkbox',
-        name: 'license',
-        message: 'What kind of license should your project have?',
-        choices: ['Apache 2.0', 'MIT', 'GNU GPL v3.0'],
-        default: 'MIT'
-      },  
-   ]);ddd
+//Questions
+const questions = [
+  { 
+  type: 'input',
+  name: 'title',
+  message: 'What is your repository name?',
+  validate: (answer) => {
+  // (answer === '') ? 'Please enter a valid title' : 'true';
+    if (answer === '') { return 'Please enter a valid title'}; return true}
+  },
+  { 
+  type: 'editor', //using the vim 
+  name: 'description',
+  message: 'Write your description',
+  validate: (answer) => {
+    if (answer === '') { return 'Description empty, please enter your description'}; return true}
+  }, 
+  { 
+  type: 'checkbox',
+  name: 'dependencies',
+  message: 'Technologies / Skill you using on this repository',
+  choices: ['HTML', 'CSS', 'Javascript', 'Python', 'Java'],
+  // validate: (answer) => {
+  //   if (answer === '') { return 'You had not selected your languages'}; return true}
+  },
+  { 
+  type: 'editor',
+  name: 'installation',
+  message: 'What are the steps required to install your project?',
+  validate: (answer) => {
+    if (answer === '') { return 'Installation empty, please enter your installation'}; return true}
+  }, 
+  {
+  type: 'checkbox',
+  name: 'license',
+  message: 'What kind of license should your project have?',
+  choices: ['Apache 2.0', 'MIT', 'GNU GPL v3.0'],
+  // validate: (answer) => {
+  //   if (answer === '') { return 'You had not selected your license'}; return true}
+  }
+];
+
+const promptUser = (questions) => {
+   return inquire.prompt(questions);
 };
 // Custom the readme display after you input the value from promptUser
 const generateCustomDisplay = (answers) => {
@@ -47,7 +58,9 @@ const generateCustomDisplay = (answers) => {
   }
   return response;
 };
-const generateREADME = ({ title, description, dependencies, license }) =>
+
+//template README 
+const generateREADME = ({ title, description, installation, dependencies, license }) =>
 `
 # ${title}
 
@@ -59,13 +72,20 @@ ${description}
 
 ${generateCustomDisplay(dependencies)}
 
+## installation
+
+${installation}
+
+## Contributing
+
 ## License
 
 ${generateCustomDisplay(license)}
 `;
 
+//initial
 const init = () => {
-    promptUser()
+    promptUser(questions)
       // Use writeFileSync method to use promises instead of a callback function
       .then((answers) => fs.writeFileSync('README.md', generateREADME(answers)))
       .then(() => console.log('Successfully wrote to README.md'))
